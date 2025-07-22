11741d0a619b043b5f1f840aa721ef9ad9005729
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-07-22 19:38:32 -0400

    feat: Created multisampling stage

--- VulkanRenderer/VulkanRenderer.cpp
+++ VulkanRenderer/VulkanRenderer.cpp
@@ -614,6 +614,15 @@ void VulkanRenderer::createGraphicsPipeline()
 	rasterizer.depthBiasClamp = 0.0f;
 	rasterizer.depthBiasSlopeFactor = 0.0f;
 
+	//Multisampling
+	VkPipelineMultisampleStateCreateInfo multisampling{};
+	multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+	multisampling.sampleShadingEnable = VK_FALSE;
+	multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+	multisampling.minSampleShading = 1.0f;
+	multisampling.pSampleMask = nullptr;
+	multisampling.alphaToCoverageEnable = VK_FALSE;
+	multisampling.alphaToOneEnable = VK_FALSE;
 
 	//Cleanup shader modules
 	vkDestroyShaderModule(mDevice, fragShaderModule, nullptr);
