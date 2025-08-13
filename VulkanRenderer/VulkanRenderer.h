a6d5c53153fdbc1010f2d182b4ff4cea2da217ed
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-08-13 09:46:18 -0400

    feat: Created attribute descriptions for position and color

--- VulkanRenderer/VulkanRenderer.h
+++ VulkanRenderer/VulkanRenderer.h
@@ -16,6 +16,7 @@
 #include <map>
 #include <set>
 #include <optional>
+#include <array>
 #include <cstdint>
 #include <limits>
 #include <algorithm>
@@ -83,6 +84,25 @@ struct Vertex
 		//Use instance here for instanced rendering
 		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
+	{
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+		//Position description
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+		//Color description
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+		return attributeDescriptions;
 	}
 };
 
